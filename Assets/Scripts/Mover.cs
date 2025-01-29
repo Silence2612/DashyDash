3a1fe2346a9752c8a50c687b8b7e4d9b086ff324
Vanshit Shivani <vanshitshivani666@gmail.com>
2025-01-29 17:12:52 +0530

    mover

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-
+    [SerializeField] float MoveSpeed = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,10 +11,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-     float X_value = Input.GetAxis("Horizontal");
+
+     float X_value = Input.GetAxis("Horizontal")*Time.deltaTime*MoveSpeed;
      float Y_value = 0.0f;
-     float Z_value = Input.GetAxis("Vertical");;
+     float Z_value = Input.GetAxis("Vertical")*Time.deltaTime*MoveSpeed;
 
         transform.Translate(X_value , Y_value , Z_value );
     }
