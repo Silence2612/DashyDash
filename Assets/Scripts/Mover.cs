0c82d2a2d4bb04121605563f26bc14aabf4c71d7
Vanshit Shivani <vanshitshivani666@gmail.com>
2025-01-29 17:40:29 +0530

    cleaned the code

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -12,16 +12,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    PlayerMovement();
 
-     float X_value = Input.GetAxis("Horizontal")*Time.deltaTime*MoveSpeed;
-     float Y_value = 0.0f;
-     float Z_value = Input.GetAxis("Vertical")*Time.deltaTime*MoveSpeed;
-
-        transform.Translate(X_value , Y_value , Z_value );
     }
 
     void PrintInstruction(){
         Debug.Log("Game Started...");
         Debug.Log("Get to the finish line to win the game while avoiding the obstacles...");
+    }
+
+    void PlayerMovement(){
+     float X_value = Input.GetAxis("Horizontal")*Time.deltaTime*MoveSpeed;
+     float Y_value = 0.0f;
+     float Z_value = Input.GetAxis("Vertical")*Time.deltaTime*MoveSpeed;
+
+        transform.Translate(X_value , Y_value , Z_value );
     }
 }
